564bce0b7591b35ab456e267e4a1cc188f8249f7
woeunjung <leena09123@naver.com>
2022-02-13 22:55:20 +0900

    연습용 commit입니다

---
+++ Card/app/src/main/java/com/example/card/UpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateActivity extends AppCompatActivity {
+//commit 잘되는지 확인 중입니다
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
